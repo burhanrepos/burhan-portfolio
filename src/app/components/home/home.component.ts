e2415375a08027ccf62523b812f25652c7cc7440
Burhan Shaheen <hafiz.burhan834@gmail.com>
2023-06-01 16:31:42 +0500

    Added project and more project pages

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private analyticsService: AnalyticsService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.analyticsService.sendAnalyticPageView("/inicio", "Se entro a inicio")
+  }
+
 
 }
