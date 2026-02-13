d2a987962ca10a904afaa35b56660f79be043672
Burhan Shaheen <burhan.shaheen@upstartcommerce.com>
2026-02-13 19:45:39 +0500

    Upgrade to Angular v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,39 +11,30 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'
-import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 
 export function HttpLoaderFactory(http: HttpClient){
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    /* ArchiveComponent */
-  ],
-  imports: [
-    BrowserAnimationsModule,
-
-    HomeModule,
-    GeneralModule,
-
-    // AnimateOnScrollModule.forRoot(),
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    NgxGoogleAnalyticsModule.forRoot(environment.trackAnalyticID),
-    TranslateModule.forRoot({
-      loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      }
-    })
-  ],
-  providers: [TranslateService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        /* ArchiveComponent */
+    ],
+    bootstrap: [AppComponent], imports: [BrowserAnimationsModule,
+        HomeModule,
+        GeneralModule,
+        // AnimateOnScrollModule.forRoot(),
+        BrowserModule,
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+        NgxGoogleAnalyticsModule.forRoot(environment.trackAnalyticID),
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })], providers: [TranslateService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
